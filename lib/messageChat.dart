3d9aeab29317e326515b0ff30bd5913997de32d2
Lans12345 <olanalans12345@gmail.com>
2024-10-14 08:07:04 +0800

    added send location func

--- lib/messageChat.dart
+++ lib/messageChat.dart
@@ -1,6 +1,9 @@
 import 'dart:typed_data';
 import 'package:flutter/foundation.dart'; // for kIsWeb
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
 import 'package:flutter_mobile_whiskerway/cons.dart';
 import 'package:flutter_mobile_whiskerway/viewpets.dart';
 import 'package:get_storage/get_storage.dart';
@@ -8,6 +11,7 @@ import 'package:image_picker/image_picker.dart'; // for picking images
 import 'package:flutter_mobile_whiskerway/home.dart';
 import 'package:flutter_mobile_whiskerway/login.dart';
 import 'package:flutter_mobile_whiskerway/profilePage.dart';
+import 'package:intl/intl.dart';
 import 'package:mongo_dart/mongo_dart.dart' as mdb;
 
 class ChatScreen extends StatefulWidget {
@@ -18,56 +22,161 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    super.initState();
+    determinePosition();
+    getChats();
+  }
+
+  Future<Position> determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
+  }
+
   bool hasLoaded = false;
 
   List chats = [];
   final box = GetStorage();
   Future<void> getChats() async {
+    try {
+      var db = await mdb.Db.create(MONGO_URL);
+      await db.open();
+
+      var collection = db.collection('chats');
+
+      // Example query to find pets with a specific condition
+
+      // Fetch documents that match the query
+      var pets = await collection.find().toList();
+
+      setState(() {
+        messages = pets;
+        hasLoaded = true;
+      });
+
+      // Print or process the results
+
+      await db.close();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  // List to store messages (both text and image)
+  List<Map<String, dynamic>> messages = [];
+
+  // Function to handle text message submission
+  void _handleSendMessage(String message) async {
     var db = await mdb.Db.create(MONGO_URL);
     await db.open();
 
     var collection = db.collection('chats');
-
-    // Example query to find pets with a specific condition
-    var query = mdb.where
-        .eq('email', box.read('email')); // Replace with your query condition
-
-    // Fetch documents that match the query
-    var pets = await collection.find(query).toList();
-
-    setState(() {
-      chats = pets;
-      hasLoaded = true;
+    await collection.insertOne({
+      'email': box.read('email'),
+      'data': message,
+      'timestamp': DateFormat('yyyy-MM-dd hh:mm a').format(DateTime.now()),
+      'updatedAt': DateTime.now(),
+      'type': 'text',
     });
-
-    // Print or process the results
-
-    await db.close();
-  }
-
-  // List to store messages (both text and image)
-  final List<Map<String, dynamic>> messages = [];
-
-  // Function to handle text message submission
-  void _handleSendMessage(String message) {
     setState(() {
       messages.add({
         'type': 'text',
         'data': message,
-        'timestamp': '9:45', // You can update this to get real-time
+        'timestamp': DateFormat('yyyy-MM-dd hh:mm a')
+            .format(DateTime.now()), // You can update this to get real-time
       });
+      hasLoaded = false;
     });
+
+    getChats();
+  }
+
+  void _handleSendLocation(String message) async {
+    var db = await mdb.Db.create(MONGO_URL);
+    await db.open();
+
+    var collection = db.collection('chats');
+    await collection.insertOne({
+      'email': box.read('email'),
+      'data': message,
+      'timestamp': DateFormat('yyyy-MM-dd hh:mm a').format(DateTime.now()),
+      'updatedAt': DateTime.now(),
+      'type': 'location',
+      'lat': message.toString().split(':')[2].split(',')[0],
+      'long': message.toString().split(':')[3],
+    });
+
+    setState(() {
+      messages.add({
+        'lat': message.toString().split(':')[2].split(',')[0],
+        'long': message.toString().split(':')[3],
+        'type': 'location',
+        'data': message,
+        'timestamp': DateFormat('yyyy-MM-dd hh:mm a')
+            .format(DateTime.now()), // You can update this to get real-time
+      });
+      hasLoaded = false;
+    });
+
+    getChats();
   }
 
   // Function to handle image submission (as bytes)
-  void _handleSendImage(Uint8List imageBytes) {
+  void _handleSendImage(Uint8List imageBytes) async {
+    var db = await mdb.Db.create(MONGO_URL);
+    await db.open();
+
+    var collection = db.collection('chats');
+    await collection.insertOne({
+      'email': box.read('email'),
+      'data': '',
+      'timestamp': DateFormat('yyyy-MM-dd hh:mm a').format(DateTime.now()),
+      'updatedAt': DateTime.now(),
+      'type': 'image',
+    });
     setState(() {
       messages.add({
         'type': 'image',
-        'data': imageBytes,
-        'timestamp': '9:46', // You can update this to get real-time
+        'data': imageBytes.toString(),
+        'timestamp': DateFormat('yyyy-MM-dd hh:mm a').format(DateTime.now()),
       });
+      hasLoaded = false;
     });
+
+    getChats();
   }
 
   @override
@@ -158,6 +267,7 @@ class _ChatScreenState extends State<ChatScreen> {
               child: CircularProgressIndicator(),
             ),
       bottomNavigationBar: ChatInputBar(
+        onSendLocation: _handleSendLocation,
         onSendMessage: _handleSendMessage,
         onSendImage: _handleSendImage,
       ), // Pass callbacks for sending text and images
@@ -169,12 +279,13 @@ class _ChatScreenState extends State<ChatScreen> {
 class ChatInputBar extends StatefulWidget {
   final Function(String) onSendMessage;
   final Function(Uint8List) onSendImage; // Callback for sending images
+  final Function(String) onSendLocation; // Callback for sending images
 
-  const ChatInputBar({
-    super.key,
-    required this.onSendMessage,
-    required this.onSendImage,
-  });
+  const ChatInputBar(
+      {super.key,
+      required this.onSendMessage,
+      required this.onSendImage,
+      required this.onSendLocation});
 
   @override
   State<ChatInputBar> createState() => _ChatInputBarState();
@@ -208,12 +319,30 @@ class _ChatInputBarState extends State<ChatInputBar> {
     }
   }
 
+  void _sendLocation() async {
+    const LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+
+    Position position =
+        await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+
+    widget.onSendLocation(
+        'Shared Location: Lat: ${position.latitude}, Long: ${position.longitude}');
+    _controller.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
       child: Row(
         children: [
+          IconButton(
+            icon: const Icon(Icons.location_on_rounded),
+            onPressed: _sendLocation, // Trigger image picker
+          ),
           IconButton(
             icon: const Icon(Icons.image),
             onPressed: _pickImage, // Trigger image picker
@@ -259,6 +388,59 @@ class ChatMessages extends StatelessWidget {
           return ChatImageBubble(
             imageBytes: message['data'], // image data as bytes
             timestamp: message['timestamp'],
+          );
+        } else if (message['type'] == 'location') {
+          // If the message is an image
+          return GestureDetector(
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return Dialog(
+                    child: SizedBox(
+                      height: 300,
+                      width: 300,
+                      child: FlutterMap(
+                        options: MapOptions(
+                          initialCenter: LatLng(
+                              double.parse(message['data']
+                                  .toString()
+                                  .split(':')[2]
+                                  .split(',')[0]),
+                              double.parse(
+                                  message['data'].toString().split(':')[3])),
+                          initialZoom: 18,
+                        ),
+                        children: [
+                          TileLayer(
+                            urlTemplate:
+                                'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                            userAgentPackageName: 'com.example.app',
+                          ),
+                          CircleLayer(circles: [
+                            CircleMarker(
+                                point: LatLng(
+                                    double.parse(message['data']
+                                        .toString()
+                                        .split(':')[2]
+                                        .split(',')[0]),
+                                    double.parse(message['data']
+                                        .toString()
+                                        .split(':')[3])),
+                                radius: 5,
+                                color: Colors.red),
+                          ]),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+            child: ChatBubble(
+              text: message['data'],
+              timestamp: message['timestamp'],
+            ),
           );
         }
         return const SizedBox.shrink();
